b6b2e7b139792a9a5952324d66ec6fae87c3d77e
Diablo <el_diablo76@msn.com>
2023-05-02 16:49:50 +0200

    Modif sur Mac

--- ClipboardToCloud.py
+++ ClipboardToCloud.py
@@ -2,7 +2,7 @@
 # Created by Diablo76 on 14/02/2023 -- 07:41:27.
 # ClipboardToCloud est un script qui permet de récupérer le contenu
 # du presse-papier d'un ordinateur à un autre.
-# Bruno Fiquet.
+# Bruno Fiquet le 02/05.
 
 import os
 import sys
