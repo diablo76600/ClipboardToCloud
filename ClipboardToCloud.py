9eeb00c8b354e62726544313ddcc0bed650ff69b
Bruno <el_diablo76@msn.com>
2023-05-08 08:31:12 +0200

    Modif

--- ClipboardToCloud.py
+++ ClipboardToCloud.py
@@ -1,7 +1,10 @@
 # -*- Coding: utf-8 -*-
 # Created by Diablo76 on 14/02/2023 -- 07:41:27.
 # ClipboardToCloud est un script qui permet de récupérer le contenu
-# du presse-papier d'un ordinateur à un autre.
+# du presse-papier d'un ordinateur à un autre 
+# sans tenir compte de l'OS et du réseau.
+# Par défaut, il fonctionne avec Dropbox mais il peut être adapté
+# pour d'autre Cloud (Google Drive etc...)
 
 import os
 import sys
