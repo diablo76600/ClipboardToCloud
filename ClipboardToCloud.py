fc174fa88cc7827f14b0ce62453fcc110a5f19b3
Bruno <el_diablo76@msn.com>
2023-06-06 13:37:42 +0200

    modified:   ClipboardToCloud.py

--- ClipboardToCloud.py
+++ ClipboardToCloud.py
@@ -9,6 +9,7 @@
 import os
 import sys
 
+sys.path.append(os.path.dirname(os.path.abspath(__file__)))
 
 from typing import Union
 from Modules.service_directory_file import ServiceDirectoryAndFile, DirectoryError
