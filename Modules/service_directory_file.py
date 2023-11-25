b52fa4955aeff17889cfc326ceb9eb2ba6cee98c
Bruno <el_diablo76@msn.com>
2023-11-25 22:45:06 +0100

    Modif

--- Modules/service_directory_file.py
+++ Modules/service_directory_file.py
@@ -2,6 +2,7 @@ import sys
 import os
 import time
 from pathlib import Path
+from PyQt5.QtCore import QFile
 
 
 class DirectoryError(Exception):
@@ -57,6 +58,7 @@ class ServiceDirectoryAndFile:
                 with open(self.path_file, "rb") as file:
                     return file.read()
             except (FileNotFoundError, PermissionError):
+                print(QFile(self.path_file).permissions())
                 time.sleep(0.1)
                 self.manager.watcher.addPath(self.path_file)
         
